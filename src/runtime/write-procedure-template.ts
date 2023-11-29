9bf19bb30c653c2048f33ba062991c9220efcbed
Kehwar <erickkwr@gmail.com>
2023-11-29 14:35:27 -0500

    refactor: format

--- src/runtime/write-procedure-template.ts
+++ src/runtime/write-procedure-template.ts
@@ -1,5 +1,6 @@
 import { addImports, addServerImports, addTemplate, addTypeTemplate, createResolver } from '@nuxt/kit'
 import _ from 'lodash'
+import dedent from 'dedent'
 import type { Options } from './options'
 
 const safefilename = 'trpc-auto-procedure'
@@ -8,11 +9,11 @@ const virtualFilename = `#trpcAutoProcedure`
 
 export function writeProcedureTemplateFiles(options: Options) {
     const resolver = createResolver(options.cwd)
-    const content = [
-        `import createTRPCApp from '${resolver.resolve(options.inject.app)}'`,
-        `export function ${varName}(){ return createTRPCApp().procedure }`,
-        `export default ${varName}`,
-    ].join('\n')
+    const content = dedent`
+        import createTRPCApp from '${resolver.resolve(options.inject.app)}'
+        export function ${varName}(){ return createTRPCApp().procedure }
+        export default ${varName}
+    `
     addTemplate({
         filename: `${safefilename}.ts`,
         write: true,
@@ -22,12 +23,11 @@ export function writeProcedureTemplateFiles(options: Options) {
     })
     addTypeTemplate({
         filename: `types/${safefilename}.d.ts`,
-        getContents: () =>
-            [
-                `declare module '${virtualFilename}' {`,
-                `const ${varName}: typeof import('../${safefilename}').${varName}`,
-                '}',
-            ].join('\n'),
+        getContents: () => dedent`
+            declare module '${virtualFilename}' {
+                const ${varName}: typeof import('../${safefilename}').${varName}
+            }
+        `,
     })
     options.nuxt.hook('nitro:config', (nitroConfig) => {
         nitroConfig.virtual = nitroConfig.virtual || {}
