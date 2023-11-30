1149c7ca6e7188c36ab2d12b5aeab3c3e2aebb03
Kehwar <erickkwr@gmail.com>
2023-11-30 10:38:30 -0500

    refactor simplify

--- playground/fixtures/trpc/src/client-plugin.ts
+++ playground/fixtures/trpc/src/client-plugin.ts
@@ -1,11 +1,21 @@
 import { createTRPCNuxtClient, httpBatchLink } from 'trpc-nuxt/client'
-import { TRPCDataTransformer } from './data-transformer'
+import _ from 'lodash'
+import superjson from 'superjson'
 import type { TRPCRoutes } from './server-handler'
+
+const dataTransformer = {
+    input: superjson,
+    output: {
+        serialize: _.noop,
+        // eslint-disable-next-line no-eval
+        deserialize: (object: unknown) => eval(`(${object})`),
+    },
+}
 
 export const TRPCClientPlugin = defineNuxtPlugin(() => {
     const headers = useRequestHeaders()
     const client = createTRPCNuxtClient<TRPCRoutes>({
-        transformer: TRPCDataTransformer,
+        transformer: dataTransformer,
         links: [
             httpBatchLink({
                 url: '/api/trpc',
--- playground/fixtures/trpc/src/server-handler.ts
+++ playground/fixtures/trpc/src/server-handler.ts
@@ -1,13 +1,22 @@
 import { initTRPC } from '@trpc/server'
 import _ from 'lodash'
 import { createNuxtApiHandler } from 'trpc-nuxt'
-import { TRPCDataTransformer } from './data-transformer'
+import { uneval } from 'devalue'
+import superjson from 'superjson'
 
 import { sayGoodbye } from '~/features/landing/src/say-goodbye.trpc'
 import { sayTest } from '~/features/dev/src/test.trpc'
 import { sayHello } from '~/features/landing/src/say-hello.trpc'
 
-const app = initTRPC.create({ transformer: TRPCDataTransformer })
+const dataTransformer = {
+    input: superjson,
+    output: {
+        serialize: (object: unknown) => uneval(object),
+        deserialize: _.noop,
+    },
+}
+
+const app = initTRPC.create({ transformer: dataTransformer })
 const router = app.router
 const procedure = app.procedure
 
