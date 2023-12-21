e6d404fe6be661664ba7a660351a56d3744ea5de
Kehwar <erickkwr@gmail.com>
2023-12-21 10:54:58 -0500

    feat: allow using any arbitrary name for the named export for remote functions

--- src/runtime/add-transformer-plugin.ts
+++ src/runtime/add-transformer-plugin.ts
@@ -2,6 +2,7 @@ import { addVitePlugin } from '@nuxt/kit'
 import { createFilter } from '@rollup/pluginutils'
 import _ from 'lodash'
 import type { Plugin } from 'vite'
+import { init, parse } from 'es-module-lexer'
 import type { Options } from './options'
 import type { TRPCProcedure } from './parse-procedure-path'
 import { parseProcedurePath } from './parse-procedure-path'
@@ -11,11 +12,11 @@ export function addTransformerPlugin(options: Options) {
     const plugin: Plugin = {
         name: 'vite-plugin-trpc',
         enforce: 'post',
-        transform(_code, id, _opts) {
+        async transform(code, id, _opts) {
             if (!filter(id))
                 return
             const procedure = parseProcedurePath(id, options)
-            const result = transformExportsToTRPCCalls(procedure, options)
+            const result = await transformExportsToTRPCCalls(code, procedure, options)
             return {
                 code: result,
             }
@@ -23,8 +24,11 @@ export function addTransformerPlugin(options: Options) {
     }
     addVitePlugin(plugin)
 }
-function transformExportsToTRPCCalls({ procedureName, routerPathName, baseName, action }: TRPCProcedure, options: Options) {
+async function transformExportsToTRPCCalls(src: string, { procedureName, routerPathName, baseName, action }: TRPCProcedure, options: Options) {
     if (action === 'error')
         throw new Error(`Could not determine action for ${baseName}`)
-    return `export const ${procedureName} = (...args) => useNuxtApp().$${options.client.alias}.${routerPathName}.${procedureName}.${action}(args)`
+    await init
+    const [, exports] = parse(src)
+    const exportList = exports.map(e => e.n).filter(e => e !== 'default')
+    return exportList.map(e => `export const ${e} = (...args) => useNuxtApp().$${options.client.alias}.${routerPathName}.${procedureName}.${action}(args)`).join('\n')
 }
